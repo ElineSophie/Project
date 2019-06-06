a80699461dd1de60dc8b2f44a297642b041fc20f
ElineSophie <soof1994@gmail.com>
2019-06-06 15:47:46 +0200

    datamap

--- static/js/datamaps.js
+++ static/js/datamaps.js
@@ -1,38 +1,49 @@
-var width = 960;
-var height = 550;
+window.onload = function(){
+  datamaps()
+}
 
-var colour = d3.scale.category20();
+function datamaps(){
+  var width = 960;
+  var height = 550;
 
-var projection = d3.geo.mercator()
-    .scale(1)
-    .translate([0, 0]);
+  var colour = d3v3.scale.category20();
 
-var path = d3.geo.path()
-    .projection(projection);
+  var projection = d3v3.geo.mercator()
+      .scale(1)
+      .translate([0, 0]);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  var path = d3v3.geo.path()
+      .projection(projection);
 
-d3.json("../data/nld.json", function(error, nld) {
+  var svg = d3v5.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-    var l = topojson.feature(nld, nld.objects.subunits).features[3],
-        b = path.bounds(l),
-        s = .2 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
-        t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+  d3v3.json("../../data/netherlands/nld.json", function(error, nld) {
 
-    projection
-        .scale(s)
-        .translate(t);
+    console.log(20);
 
-    svg.selectAll("path")
-        .data(topojson.feature(nld, nld.objects.subunits).features).enter()
-        .append("path")
-        .attr("d", path)
-        .attr("fill", function(d, i) {
-            return colour(i);
-        })
-        .attr("class", function(d, i) {
-            return d.properties.name;
-        });
-});
+      var l = topojson.feature(nld, nld.objects.subunits).features[3],
+          b = path.bounds(l),
+          s = .2 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+          t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+
+      projection
+          .scale(s)
+          .translate(t);
+
+      svg.selectAll("path")
+          .data(topojson.feature(nld, nld.objects.subunits).features).enter()
+          .append("path")
+          .attr("d", path)
+          .attr("fill", function(d, i) {
+            console.log(20);
+              return colour(i);
+          })
+          .attr("class", function(d, i) {
+              return d.properties.name;
+          });
+  });
+
+
+}
